9c523ca92ff50fbb90a5e69e19c59d79861ff523
dimok <zhasulanov04@list.ru>
2023-10-08 14:46:14 +0600

    Add viewModel and connect

--- app/src/main/java/com/dexciuq/shoppinglist/domain/ProductRepository.kt
+++ app/src/main/java/com/dexciuq/shoppinglist/domain/ProductRepository.kt
@@ -1,7 +1,9 @@
 package com.dexciuq.shoppinglist.domain
 
+import androidx.lifecycle.LiveData
+
 interface ProductRepository {
-    fun getProductList(): List<Product>
+    fun getProductList(): LiveData<List<Product>>
     fun getProduct(id: Int): Product
     fun addProduct(product: Product)
     fun updateProduct(product: Product)
--- app/src/main/java/com/dexciuq/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/dexciuq/shoppinglist/presentation/MainActivity.kt
@@ -2,11 +2,23 @@ package com.dexciuq.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.dexciuq.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        viewModel.productList.observe(this) {
+
+        }
     }
 }
