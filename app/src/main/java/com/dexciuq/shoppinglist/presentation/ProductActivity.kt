7b3c5cf1ac22565f767ece1281967d2ab5e8735d
dimok <zhasulanov04@list.ru>
2023-12-05 22:18:24 +0600

    Make product fragment

--- app/src/main/java/com/dexciuq/shoppinglist/presentation/ProductActivity.kt
+++ app/src/main/java/com/dexciuq/shoppinglist/presentation/ProductActivity.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.doOnTextChanged
-import androidx.lifecycle.ViewModelProvider
 import com.dexciuq.shoppinglist.R
 import com.dexciuq.shoppinglist.databinding.ActivityProductBinding
 import com.dexciuq.shoppinglist.domain.Product
@@ -13,85 +11,15 @@ import com.dexciuq.shoppinglist.domain.Product
 class ProductActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityProductBinding
-    private lateinit var viewModel: ProductViewModel
-
-    private var mode: String = ""
+    private var mode: String = MODE_UNKNOWN
     private var id: Int = Product.UNDEFINED_ID
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        parseIntent()
         binding = ActivityProductBinding.inflate(layoutInflater)
-        viewModel = ViewModelProvider(this)[ProductViewModel::class.java]
         setContentView(binding.root)
-
-        when (mode) {
-            MODE_ADD -> launchAddMode()
-            MODE_EDIT -> launchEditMode()
-        }
-        setupTextWatchers()
-        setupErrorObservers()
+        parseIntent()
+        launchRightFragment()
     }
-
-    private fun launchEditMode() {
-        viewModel.getProduct(id)
-
-        viewModel.product.observe(this) {
-            binding.etName.setText(it.name)
-            binding.etCount.setText(it.quantity.toString())
-        }
-
-        binding.saveButton.setOnClickListener {
-            viewModel.updateProduct(
-                nameInput = binding.etName.text?.toString(),
-                quantityInput = binding.etCount.text?.toString()
-            )
-        }
-    }
-
-    private fun launchAddMode() {
-        binding.saveButton.setOnClickListener {
-            viewModel.addProduct(
-                nameInput = binding.etName.text?.toString(),
-                quantityInput = binding.etCount.text?.toString()
-            )
-        }
-    }
-
-    private fun setupErrorObservers() {
-        viewModel.nameInputError.observe(this) {
-            val message = if (it) {
-                getString(R.string.error_input_name)
-            } else {
-                null
-            }
-            binding.tilName.error = message
-        }
-
-        viewModel.quantityInputError.observe(this) {
-            val message = if (it) {
-                getString(R.string.error_input_count)
-            } else {
-                null
-            }
-            binding.tilCount.error = message
-        }
-
-        viewModel.shouldCloseScreen.observe(this) {
-            finish()
-        }
-    }
-
-    private fun setupTextWatchers() {
-        binding.etName.doOnTextChanged { _, _, _, _ ->
-            viewModel.resetNameInputError()
-        }
-
-        binding.etCount.doOnTextChanged { _, _, _, _ ->
-            viewModel.resetQuantityInputError()
-        }
-    }
-
 
     private fun parseIntent() {
         if (!intent.hasExtra(EXTRA_SCREEN_MODE)) {
@@ -107,16 +35,29 @@ class ProductActivity : AppCompatActivity() {
                 }
                 id = intent.getIntExtra(EXTRA_PRODUCT_ID, Product.UNDEFINED_ID)
             }
+
             else -> error("unknown mode")
         }
+    }
 
+    private fun launchRightFragment() {
+        val fragment = when (mode) {
+            MODE_ADD -> ProductFragment.newInstanceAddMode()
+            MODE_EDIT -> ProductFragment.newInstanceEditMode(id)
+            else -> error("unknown mode")
+        }
+        supportFragmentManager.beginTransaction()
+            .add(R.id.product_container, fragment)
+            .commit()
     }
 
     companion object {
         private const val EXTRA_SCREEN_MODE = "extra_screen_mode"
         private const val EXTRA_PRODUCT_ID = "extra_product_id"
+
         private const val MODE_EDIT = "mode_edit"
         private const val MODE_ADD = "mode_add"
+        private const val MODE_UNKNOWN = "mode_unknown"
 
         fun newIntentAddMode(context: Context): Intent {
             val intent = Intent(context, ProductActivity::class.java)
---
+++ app/src/main/java/com/dexciuq/shoppinglist/presentation/ProductFragment.kt
@@ -0,0 +1,157 @@
+package com.dexciuq.shoppinglist.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.dexciuq.shoppinglist.R
+import com.dexciuq.shoppinglist.databinding.FragmentProductBinding
+import com.dexciuq.shoppinglist.domain.Product
+
+class ProductFragment : Fragment() {
+
+    private val binding by lazy { FragmentProductBinding.inflate(layoutInflater) }
+    private lateinit var viewModel: ProductViewModel
+
+    private var mode: String = MODE_UNKNOWN
+    private var id: Int = Product.UNDEFINED_ID
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseParams()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[ProductViewModel::class.java]
+        setupScreenMode()
+        setupTextWatchers()
+        setupErrorObservers()
+    }
+
+    private fun parseParams() {
+        if (arguments?.containsKey(SCREEN_MODE) != true) {
+            error("param screen mode is absent")
+        }
+
+        mode = arguments?.getString(SCREEN_MODE) ?: MODE_UNKNOWN
+        if (mode != MODE_ADD && mode != MODE_EDIT) {
+            error("unknown mode")
+        }
+
+        if (mode == MODE_EDIT) {
+            if (arguments?.containsKey(PRODUCT_ID) != true) {
+                error("param product id is absent")
+            }
+
+            id = arguments?.getInt(PRODUCT_ID) ?: Product.UNDEFINED_ID
+            if (id == Product.UNDEFINED_ID) {
+                error("param product id is absent")
+            }
+        }
+    }
+
+    private fun setupScreenMode() {
+        when (mode) {
+            MODE_ADD -> launchAddMode()
+            MODE_EDIT -> launchEditMode()
+        }
+    }
+
+    private fun launchEditMode() {
+        viewModel.getProduct(id)
+
+        viewModel.product.observe(viewLifecycleOwner) {
+            binding.etName.setText(it.name)
+            binding.etCount.setText(it.quantity.toString())
+        }
+
+        binding.saveButton.setOnClickListener {
+            viewModel.updateProduct(
+                nameInput = binding.etName.text?.toString(),
+                quantityInput = binding.etCount.text?.toString()
+            )
+        }
+    }
+
+    private fun launchAddMode() {
+        binding.saveButton.setOnClickListener {
+            viewModel.addProduct(
+                nameInput = binding.etName.text?.toString(),
+                quantityInput = binding.etCount.text?.toString()
+            )
+        }
+    }
+
+    private fun setupErrorObservers() {
+        viewModel.nameInputError.observe(viewLifecycleOwner) {
+            val message = if (it) {
+                getString(R.string.error_input_name)
+            } else {
+                null
+            }
+            binding.tilName.error = message
+        }
+
+        viewModel.quantityInputError.observe(viewLifecycleOwner) {
+            val message = if (it) {
+                getString(R.string.error_input_count)
+            } else {
+                null
+            }
+            binding.tilCount.error = message
+        }
+
+        viewModel.shouldCloseScreen.observe(viewLifecycleOwner) {
+            activity?.onBackPressed()
+        }
+    }
+
+    private fun setupTextWatchers() {
+        binding.etName.doOnTextChanged { _, _, _, _ ->
+            viewModel.resetNameInputError()
+        }
+
+        binding.etCount.doOnTextChanged { _, _, _, _ ->
+            viewModel.resetQuantityInputError()
+        }
+    }
+
+    companion object {
+        private const val SCREEN_MODE = "screen_mode"
+        private const val PRODUCT_ID = "product_id"
+
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_ADD = "mode_add"
+        private const val MODE_UNKNOWN = "mode_unknown"
+
+        fun newInstanceAddMode(): ProductFragment {
+            return ProductFragment().apply {
+                arguments = bundleOf(
+                    SCREEN_MODE to MODE_ADD,
+                )
+            }
+        }
+
+        fun newInstanceEditMode(id: Int): ProductFragment {
+            return ProductFragment().apply {
+                arguments = bundleOf(
+                    SCREEN_MODE to MODE_EDIT,
+                    PRODUCT_ID to id
+                )
+            }
+        }
+    }
+}
