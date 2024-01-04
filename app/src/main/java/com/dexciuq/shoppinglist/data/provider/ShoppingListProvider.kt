9dfc74a62b65433bcaab0e5e1188ce58cb6de539
dimok <zhasulanov04@list.ru>
2024-01-04 16:01:35 +0600

    Add UriMatcher

--- app/src/main/java/com/dexciuq/shoppinglist/data/provider/ShoppingListProvider.kt
+++ app/src/main/java/com/dexciuq/shoppinglist/data/provider/ShoppingListProvider.kt
@@ -2,11 +2,17 @@ package com.dexciuq.shoppinglist.data.provider
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 import android.util.Log
 
 class ShoppingListProvider : ContentProvider() {
+
+    private val uriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
+        addURI("com.dexciuq.shoppinglist", "products", CODE_QUERY_GET_PRODUCTS)
+        addURI("com.dexciuq.shoppinglist", "products/#", CODE_QUERY_GET_PRODUCT_BY_ID)
+    }
 
     override fun onCreate(): Boolean {
         return true
@@ -19,7 +25,17 @@ class ShoppingListProvider : ContentProvider() {
         selectionArgs: Array<out String>?,
         sortOrder: String?
     ): Cursor? {
-        Log.d("ShoppingListProvider", "query: $uri")
+        val code = uriMatcher.match(uri)
+        when (code) {
+            CODE_QUERY_GET_PRODUCTS -> {
+
+            }
+            CODE_QUERY_GET_PRODUCT_BY_ID -> {
+
+            }
+        }
+
+        Log.d("ShoppingListProvider", "query: $uri $code")
         return null
     }
 
@@ -42,5 +58,10 @@ class ShoppingListProvider : ContentProvider() {
         selectionArgs: Array<out String>?
     ): Int {
         TODO("Not yet implemented")
+    }
+
+    companion object {
+        private const val CODE_QUERY_GET_PRODUCTS = 100
+        private const val CODE_QUERY_GET_PRODUCT_BY_ID = 101
     }
 }
